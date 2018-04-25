ff9c49db8a52cf9a1efea21fe347e3d80e65f36f
KrooEidJa <sago2408@hotmail.com>
2018-04-25 14:08:57 +0700

    Initial commit

---
+++ app/src/main/java/kd/ac/th/showkp/MainActivity.java
@@ -0,0 +1,27 @@
+package kd.ac.th.showkp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import kd.ac.th.showkp.fragment.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //  Add Fragment
+        if (savedInstanceState== null) {
+
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.contentMainFragment, new MainFragment()).commit();
+        }
+
+
+
+
+
+    }   //Main Method
+}   //Main Class
