0673814aa2f9b2fb0722c9b01645812b15a28303
KrooEidJa <sago2408@hotmail.com>
2018-04-25 16:55:34 +0700

    Replace Fragment Success 16.55 I am headage

--- app/src/main/java/kd/ac/th/showkp/fragment/RegisterFragment.java
+++ app/src/main/java/kd/ac/th/showkp/fragment/RegisterFragment.java
@@ -5,13 +5,32 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import kd.ac.th.showkp.MainActivity;
 import kd.ac.th.showkp.R;
 
 public class RegisterFragment extends Fragment{
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        Create Toolbar
+        createToolbar();
+
+
+    }   //Main Method
+
+    private void createToolbar() {
+        Toolbar toolbar = getActivity().findViewById(R.id.toolbarRegister);
+        ((MainActivity)getActivity()).setSupportActionBar(toolbar);
+
+    }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
