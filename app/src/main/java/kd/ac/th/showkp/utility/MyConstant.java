af53bbefd1176f69a087a6791e08b9fe0f2d721e
KrooEidJa <sago2408@hotmail.com>
2018-04-26 17:40:28 +0700

    26/04/61 Ha Ha Ha 17.40

--- app/src/main/java/kd/ac/th/showkp/utility/MyConstant.java
+++ app/src/main/java/kd/ac/th/showkp/utility/MyConstant.java
@@ -4,6 +4,11 @@ public class MyConstant {
 
     private String urlAddUser = "http://androidthai.in.th/bru/addUser.php";
     private String urlGetallUser = "http://androidthai.in.th/bru/getAllUser.php";
+    private String ureGetAllFood = "http://androidthai.in.th/bru/getAllFood.php";
+
+    public String getUreGetAllFood() {
+        return ureGetAllFood;
+    }
 
     public String getUrlGetallUser() {
         return urlGetallUser;
